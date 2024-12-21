a049ef2ba4d3b46b8a210b2a9bbd7600d9af9de8
SujayCH1 <suju.narayan@gmail.com>
2024-12-21 13:18:09 +0530

    login Commit 1

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Login from './components/Login'
+import { UserProvider } from './context/UserContext'
 
 const index = () => {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+    return (
+        <UserProvider>
+                <Login />
+        </UserProvider>
+    )
 }
 
 export default index
