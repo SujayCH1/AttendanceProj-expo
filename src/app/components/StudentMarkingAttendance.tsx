21d4a5a8d2a7399dc5c7c1b7e5d856d1d8bfdf94
Lalit <lalitdhembre6@gmail.com>
2025-01-07 15:22:33 +0530

    Teacher View ble setup

--- src/app/components/StudentMarkingAttendance.tsx
+++ src/app/components/StudentMarkingAttendance.tsx
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
 import { View, Button, StyleSheet } from 'react-native';
+import bleService from '../backend/bleSetup';
 
 const StudentMarkingAttendance = () => {
   const [isPressed, setIsPressed] = useState(false);
 
+  const {
+    requestPermission,
+    checking,
+  } = bleService();
+
   // Function to handle button press
   const handlePress = () => {
+    requestPermission
+    // checking(uuid);
     setIsPressed(!isPressed); // Toggle the button color
   };
 
   return (
     <View style={styles.container}>
       <Button
-        title="Mark Attendance"
+        title={isPressed ?"Stop Attendance"  : "Mark Attendance"}
         color={isPressed ? 'red' : undefined} // Change color to red when pressed
         onPress={handlePress}
       />
